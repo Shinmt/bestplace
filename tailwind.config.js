5d248dac13e29d43746d5f930f448959100badb6
Shinmt <shinminthwesmt99@gmail.com>
2023-06-27 13:15:20 +0630

    23-6-27-screensizecommit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '320px',
+        'md' : '480px',
+        'tablet' : '640px',
+        'lg' : '768px',
+        'laptop' : '1024px',
+        'desktop' : '1232px',
+        '3xl' : '1366px',
+      },
+    },
   },
   plugins: [],
 }
